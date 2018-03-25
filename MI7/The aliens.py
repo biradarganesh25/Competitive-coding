383e5a7d0ea7475468ceb21d5115cd92208df342
V. Subramani Raju <vsr625@gmail.com>
2018-03-25 16:09:01 +0530

    vsr625: update aliens.py

--- MI7/The aliens.py
+++ MI7/The aliens.py
@@ -1,8 +1,8 @@
-// Problem Name: The Aliens
+# Problem Name: The Aliens
 
-// Problem Description: You are given a string of digits, you have to find out how many digits are in the range 1 to 26
+# Problem Description: You are given a string of digits, you have to find out how many digits are in the range 1 to 26
 
-// Solution: The most simplest solution explains it the best. This is from @Annaray.
+# Solution: The most simplest solution explains it the best. This is from @Annaray.
 
 n=str(input())
 k=0
